2a7d76d57ded8e8037dc4d67d0aed0fdd1c4ee48
Lucas Saladini <lucassaladini@gmail.com>
2026-04-26 17:50:40 -0300

    (deploy): Setting Render deploy

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -4,27 +4,44 @@ import { authRoutes } from './routes/auth-routes';
 import { hobbyRoutes } from './routes/hobby-routes';
 import fastifyCookie from '@fastify/cookie';
 
-const app = fastify({ logger: true });
+const app = fastify({ 
+  logger: true,
+});
 
 app.register(fastifyCookie, {
   secret: process.env.COOKIE_SECRET,
 });
+
 app.register(cors, {
-    origin: ['http://localhost:3000'],
+    origin: (origin, cb) => {
+      const allowedOrigins = ['http://localhost:3000', process.env.FRONTEND_URL];
+      if (!origin || allowedOrigins.includes(origin)) {
+        cb(null, true);
+        return;
+      }
+      cb(new Error('Not allowed by CORS'), false);
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-})
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+});
+
 app.register(authRoutes, { prefix: '/auth' });
-app.register(hobbyRoutes, { prefix: '/api/hobbies'})
+app.register(hobbyRoutes, { prefix: '/api/hobbies'});
 
 const start = async () => {
-    try {
-        await app.listen({ port: 3333, host: '0.0.0.0' });
-        console.log('🚀 SlowPace Backend rodando em http://localhost:3333');
-    } catch (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
-};
+  try {
+    const port = Number(process.env.PORT) || 3333;
+    
+    await app.listen({ 
+      port, 
+      host: '0.0.0.0' 
+    });
+    
+    console.log(`🚀 Server rodando na porta: ${port}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+}
 
 start();
