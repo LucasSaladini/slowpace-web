33c15183dadd6c382e75cd940e2728e1ebefad3a
Lucas Saladini <lucassaladini@gmail.com>
2026-07-18 23:08:02 -0300

    front: adjusted routes  on frontend for the new API address

--- apps/mobile/src/app/services/api.ts
+++ apps/mobile/src/app/services/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333/api';
+
+const finalBaseURL = baseURL.endsWith('/api') ? baseURL : `${baseURL}/api`;
+
 export const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3333/api'
-        : '/api',
+    baseURL: finalBaseURL,
     withCredentials: true,
 });
